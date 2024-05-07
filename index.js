9c57b129079904a2cde40fd54d8c279d21fc0c44
hnkih <csjclphillips72@hotmail.com>
2024-05-07 21:44:11 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-function subsets(nums) {
-  const result = [];
-  backtrack(0, []);
-  return result;
-  function backtrack(start, current) {
-    result.push([...current]);
-    for (let i = start; i < nums.length; i++) {
-      current.push(nums[i]);
-      backtrack(i + 1, current);
-      current.pop();
-    }
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-}
+  return gcd(b, a % b);
+};
