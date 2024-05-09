cc7882d20cb73c9fbecf194b4987b61b183d9342
hnkih <csjclphillips72@hotmail.com>
2024-05-09 09:25:39 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorialRecursive(n - 1);
+function minStack() {
+  this.stack = [];
+  this.minStack = [];
+}
+minStack.prototype.push = function (x) {
+  this.stack.push(x);
+  if (
+    this.minStack.length === 0 ||
+    x <= this.minStack[this.minStack.length - 1]
+  )
+    this.minStack.push(x);
+};
+minStack.prototype.pop = function () {
+  if (this.stack.pop() === this.minStack[this.minStack.length - 1])
+    this.minStack.pop();
+};
+minStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+minStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1];
 };
