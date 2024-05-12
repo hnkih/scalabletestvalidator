80401f6c4ee8a427f46c59e5fc7d415f0230d3aa
hnkih <csjclphillips72@hotmail.com>
2024-05-12 15:44:52 +0700

    hello world

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-function lengthOfLIS(nums) {
-  if (nums.length === 0) return 0;
-  const dp = new Array(nums.length).fill(1);
-  let max = 1;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
-    }
-    max = Math.max(max, dp[i]);
+function mergeKLists(lists) {
+  if (lists.length === 0) return null;
+  while (lists.length > 1) {
+    const first = lists.shift();
+    const second = lists.shift();
+    const merged = mergeTwoLists(first, second);
+    lists.push(merged);
   }
-  return max;
+  return lists[0];
 }
